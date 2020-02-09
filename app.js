dbaa407013d71cd849b431022f5077e90b07bed0
Kwok <Khwong@ucsd.edu>
2020-02-08 20:45:11 -0800

    made the form

---
+++ app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+
+var index = require('./routes/index');
+var lost = require('./routes/lost');
+var found = require('./routes/found');
+var signin = require('./routes/signin');
+var create = require('./routes/create');
+var post = require('./routes/post');
+
+// Example route
+// var user = require('./routes/user');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('IxD secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', index.view);
+app.get('/lost', lost.viewLF);
+app.get('/found', found.viewLF);
+app.get('/signin', signin.viewLF);
+app.get('/create', create.viewLF);
+app.get('/post', post.viewLF);
+
+// Example route
+// app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
