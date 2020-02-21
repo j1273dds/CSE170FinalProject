02f7e7dbfa623d4062e5b5df9ca8aa48d869dc1b
Your name <ahs012@ucsd.edu>
2020-02-20 17:21:51 -0800

    new post found page

--- routes/add.js
+++ routes/add.js
@@ -5,8 +5,16 @@ exports.addPost = function(request, response) { 
 	var name = request.query.name;
 	var description = request.query.description;
 	var image = request.query.image;
+	var lost_found = request.query.lost_found;
 	var newPost = { "name":name , "description":description, "image":image};
+
+	if (lost_found == "lost"){
+  		data.posts.push(newPost);
+		response.render('lost',data);
+	} else {
+		console.log(lost_found);
+		datafound.posts.push(newPost);
+		response.render('lost',datafound);
+	}
 	
-   data.posts.push(newPost);
-	response.render('lost',data);
  }
