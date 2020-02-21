2500f808f869c3d9076521c71aa0d379afe21d75
Your name <ahs012@ucsd.edu>
2020-02-20 19:04:23 -0800

    facebook login

---
+++ routes/addAcc.js
@@ -0,0 +1,11 @@
+var data = require("../account.json");
+
+exports.addAcc = function(request, response) {    
+	var firstName = request.query.firstName;
+	var lastName = request.query.lastName;
+	var email = request.query.email;
+	var username = request.query.username;
+	var newPost = { "firstName":firstName , "lastName":lastName, "email":email,"username":username};
+	data.account.push(newPost);
+	response.render('account',data);
+ }
