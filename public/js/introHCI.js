e7615760bddd34e4b0758e1363675ae9c5e5eef1
Kwok <jtyue@ucsd.edu>
2020-03-03 19:14:17 -0800

    moved search bar i think

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -55,24 +55,9 @@ function init() {
 }
 
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+function showDropDown(){
+  console.log(1);
+  document.getElementByClass("dropdown-content").toggle("show");
 }
 
 // user clicks on Found
