3a003b9d7ed3e5db9fe1925703a3fb5e9dc49fad
Kwok <jtyue@ucsd.edu>
2020-03-04 15:04:04 -0800

    added font changes to homescreen

--- routes/addAcc.js
+++ routes/addAcc.js
@@ -4,8 +4,11 @@ exports.addAcc = function(request, response) {
 	var firstName = request.query.firstName;
 	var lastName = request.query.lastName;
 	var email = request.query.email;
+	var phone = request.query.phone;
+	var image = request.query.image;
 	var username = request.query.username;
-	var newPost = { "firstName":firstName , "lastName":lastName, "email":email,"username":username};
+	var newPost = { "firstName":firstName , "lastName":lastName, "email":email,"username":username,"image":image,"phone":phone};
 	mylost.account.push(newPost);
+
 	response.render('account',mylost);
 }
