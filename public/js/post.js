c785eae00a18e5effbb0f125e71737dabf899459
Kwok <jtyue@ucsd.edu>
2020-03-15 16:15:36 -0700

    ..

--- public/js/post.js
+++ public/js/post.js
@@ -15,3 +15,24 @@ function date() {
 
 }
 
+var outImage ="imagenFondo";
+function preview_2(obj)
+{
+  if (FileReader)
+  {
+    var reader = new FileReader();
+    reader.readAsDataURL(obj.files[0]);
+    reader.onload = function (e) {
+    var image=new Image();
+    image.src=e.target.result;
+		
+    // image.onload = function () {
+    //   document.getElementById(outImage).src=image.src;
+    // };
+    }
+  }
+  else
+  {
+        // Not supported
+  }
+}
